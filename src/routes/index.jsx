8e6fdf2ebefc0cc51737cd8c457485a4ebd2a65b
Jim Nielsen <jimniels@gmail.com>
2022-06-14 16:03:16 -0600

    add zero state to <Index>

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,10 @@
 export default function Index() {
-  return <div>Index</div>;
+  return (
+    <p id="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check out{" "}
+      <a href="https://reactrouter.com/">the docs at reactrouter.com</a>.
+    </p>
+  );
 }
